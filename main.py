e14baede522e1014fa991915b292e7a5bc877dea
Entiteten <sebastianlindgren@outlook.com>
2025-07-23 22:08:52 +0200

    C: updated the code to use groups

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     print("Starting Asteroids!")
     print((f"Screen width: {SCREEN_WIDTH}"))
     print((f"Screen height: {SCREEN_HEIGHT}"))
@@ -14,6 +18,7 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
 
     while True:
         clock.tick(60)
@@ -23,9 +28,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
 
 
