bc650b9739e46b078389add6596d62af9eb07f90
Entiteten <sebastianlindgren@outlook.com>
2025-07-25 12:47:28 +0200

    D: added asteroid field

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from constants import *
 from player import Player
+
 
 def main():
     pygame.init()
@@ -9,7 +12,11 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     print("Starting Asteroids!")
     print((f"Screen width: {SCREEN_WIDTH}"))
@@ -18,6 +25,7 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    AsteroidField()
 
 
     while True:
@@ -27,11 +35,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         updatable.update(dt)
         screen.fill("black")
-        for i in drawable:
-            i.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
 
